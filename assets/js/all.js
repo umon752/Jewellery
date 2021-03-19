1b9b927551b3c46aa8d26e1757d56fc658c377cb
umon752 <umon752@gmail.com>
2021-03-19 15:19:56 +0800

    Update 2021-03-19T07:19:47.788Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,10 +1,24 @@
 "use strict";
 
+var time = 1800;
 $(document).ready(function () {
-  $('.introProduct__item__sold').click(function (e) {
-    e.preventDefault();
-    $('.introProduct__item__sold').toggleClass('animate__shakeX');
-  });
+  /* 1 秒後開始執行 */
+  setTimeout(function () {
+    // 設定 loading 畫面消失
+    $('.loading').addClass('loading--fadeOut');
+    /* AOS */
+
+    AOS.init({
+      duration: 500,
+      once: true
+    });
+    /* Animate.css */
+
+    $('.introProduct__item__sold').click(function (e) {
+      e.preventDefault();
+      $('.introProduct__item__sold').toggleClass('animate__shakeX');
+    });
+  }, time);
 });
 "use strict";
 

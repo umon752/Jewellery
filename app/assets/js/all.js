45a95bbfc61ed14fedbe2edcc7f4b1aed0386332
umon752 <umon752@gmail.com>
2021-03-20 12:06:05 +0800

    修改 loading 動畫 使用 anime.js

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,11 +1,31 @@
-let time = 1800;
+const time = 2000;
+
+/* Anime */
+anime.timeline({
+    loop: true
+  })
+  .add({
+    targets: '.loading__block__img',
+    translateX: [40, 0],
+    translateZ: 0,
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 450,
+    delay: (el, i) => 500 + 30 * i
+  }).add({
+    targets: '.loading__block__img',
+    translateX: [0, -30],
+    opacity: [1, 0],
+    easing: "easeInExpo",
+    duration: 450,
+    delay: (el, i) => 100 + 30 * i
+  });
 
 $(document).ready(function () {
   /* 1 秒後開始執行 */
   setTimeout(function () {
     // 設定 loading 畫面消失
     $('.loading').addClass('loading--fadeOut');
-
 
     /* AOS */
     AOS.init({
